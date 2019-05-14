38618672e3636cfca2e331d8cf3d9b4a07cc9d75
bob <nolanplattgithub@gmail.com>
2019-05-14 11:16:49 -0700

    :smile:
    :smile:

--- src/_00_Sorting_Algorithms/QuickSorter.java
+++ src/_00_Sorting_Algorithms/QuickSorter.java
@@ -13,6 +13,8 @@ public class QuickSorter extends Sorter {
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
 		//10. call the quickSort method with 0 and the length of the array minus one
+	
+		quickSort(array, 0, array.length - 1, display);
 	}
 	
 	private void quickSort(int[] array, int low, int high, SortingVisualizer display) {
