7d6267ebb5d8973df3abd54938a0e201a11ced03
nolanplatt <league@WTS14-4.attlocal.net>
2019-05-13 18:18:38 -0700

    :smile:
    :smile:

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -2,9 +2,16 @@ package _00_Sorting_Algorithms;
 
 import java.util.Random;
 
-public class BogoSorter extends Sorter {
-	public BogoSorter() {
-		type = "Bogo";
+public class BogoSorter {
+	
+	
+	public BogoSorter() {	
+	
+	}
+	
+	public static void main(String[] args) {
+		int[] a = {10, 1, 80, 5, 80, 5, 2, 1, 5, 7, 6, 9, 23, 29, 10, 24, 56};
+		sort(a);
 	}
 	
 	// Bogo sort is a joke sorting algorithm. It is considered the most 
@@ -17,9 +24,25 @@ public class BogoSorter extends Sorter {
 	// STEP 2. Take two random elements in the array and swap them.
 	// STEP 3. Go back to step 1.
 	
-	//1. Complete the sort method using the Bogo sort algorithm. 
-	@Override
-	void sort(int[] array, SortingVisualizer display) {
+	//1. Complete the sort method using the Bogo sort algorithm.
+	
 
+	static void sort(int[] array) {
+		  for (int i = 0; i < array.length - 1; i++) {
+		        if (array[i] > array[i + 1]) {
+		            System.out.println("Not in order; swapping two random elements...");
+		            Random r = new Random();
+		            int randomkey1 = r.nextInt(array.length);
+		            int randomkey2 = r.nextInt(array.length);
+		            int key2 = array[randomkey2];
+		            array[randomkey1] = key2;
+		            key2 = array[randomkey1];
+		            
+
+		        }
+		        else {
+		        	System.out.println("Sorter completed; array is now in order.");
+		        }
+	}
 	}
 }
--- src/_00_Sorting_Algorithms/_01_SwappingDigits.java
+++ src/_00_Sorting_Algorithms/_01_SwappingDigits.java
@@ -24,20 +24,38 @@ class _01_SwappingDigits {
 	
 	//1. Complete the method so that the first two elements of the array are swapped
 	public static void swapArrayOfTwo(int[] arr) {
-		
+		int arrayTo0 = arr[0];
+		arr[0] = arr[1];
+		arr[1] = arrayTo0;
 	}
 	
 	//2. Complete the method so that it sorts the array using a bubble sort.
 	//   Iterate through the array and when you find two elements that are out
 	//   of order, swap them. Repeat this until the array is in order.
 	public static void sortIntArray(int[] arr) {
-		
+		for (int i = 0; i < arr.length; i++) {
+			int firstArray = arr[i];
+			for (int j = 0; j < arr.length - i; j++) {
+				if(firstArray> j) {
+					System.out.println(firstArray + ">" + j + " , ok");
+				}
+				else {
+					System.out.println(j + ">" + firstArray + " , swapping");
+					arr[i] = arr[j];
+					arr[j] = arr[i];
+				}
+				
+			}
+		}
 	}
 	
 	//3. Complete the method so that it finds the middle number in the array.
 	//   *Hint* it helps to sort it first.
 	//   *Double Hint* Use the method you already wrote in step 2 to sort it
 	public static int findMiddle(int[] arr) {
-		return 0;
+		
+		int middle = arr.length / 2;
+		System.out.println(middle);
+		return middle;
 	}
 }
