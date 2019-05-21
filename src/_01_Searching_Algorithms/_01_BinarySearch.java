05aa66d3c43ed8423b73cc3d7961f4ab24d12a4d
League Student <league@iMac.attlocal.net>
2019-05-20 18:26:04 -0700

    :smile:
    :smile:

--- src/_01_Searching_Algorithms/_01_BinarySearch.java
+++ src/_01_Searching_Algorithms/_01_BinarySearch.java
@@ -15,21 +15,32 @@ public class _01_BinarySearch {
 	public static int binarySearch(int[] array, int start, int end, int value) {
 		//2. if end is greater than or equal to start, then
 		//   do steps 3 - 6.
+		if(end >= start) {
+			
 		
 			//3. create an integer called mid and set it equal
 			//   to the half way point between start and end
 
+			int mid = start / end;
             //4. if the array element at mid is equal to value
             //   then return mid
- 
+			if(array[mid] == value) {
+				return mid;
+			}
             //5. if the array element at mid is greater than value
             //   then return the value returned from a call to the 
             //   binarySearch method. Pass in start and mid - 1
             //   for the end variable.
             
+			if(array[mid] > value) {
+				int val5 = binarySearch(array, start, mid - 1, value);
+				return val5;
+			}
             //6. return the value returned from a call to the binarySearch
             //   method. Use mid + 1 as the start, and pass in end.
- 
+			int val6 = binarySearch(array, mid + 1, end, value);
+			return val6;
+		}
         //7. return -1 because the value was not found
         return 0;
 	} 
--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -11,12 +11,22 @@ class _04_SearchTest {
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
+		String[] array = {"Bob", "Jerry", "Jack"};
+		_00_LinearSearch ls = new _00_LinearSearch();
+		assertEquals(2, ls.linearSearch(array, "Jack"));
+		assertEquals(-1, ls.linearSearch(array, "Nolan"));
+		assertEquals(1, ls.linearSearch(array, "Jerry"));
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		int[] array = {1, 2, 3, 4};
+		_01_BinarySearch bs = new _01_BinarySearch();
+		assertEquals(2, bs.binarySearch(array, 1, 4, 2));
+		assertEquals(3, bs.binarySearch(array, 1, 4, 3));
+		assertEquals(4, bs.binarySearch(array, 1, 4, 4));
 	}
 	
 	@Test
