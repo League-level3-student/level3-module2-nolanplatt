f55527d5db67ada38c12cae78e4b239abbd0d488
nolanplatt <league@WTS14-4.attlocal.net>
2019-06-03 18:25:22 -0700

    :smile:
    :smile:

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,78 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
+		for (int i = 0; i < eggs.size(); i++) {
+			if(eggs.get(i).equals("cracked")) {
+				return i;
+			}
+		}
 		return 0;
 	}
+	
+	public static int countPearls(List<Boolean> oysters) {
+		
+		int count = 0;
+		for (int i = 0; i < oysters.size() ;i++) {
+			if(oysters.get(i)) {
+				count++;
+			}
+		}
+		
+		return count;
+	}
+	
+	public static Double findTallest(List<Double> peeps) {
+		Double heighest = 0.0;
+		
+		for (int i = 0; i < peeps.size(); i++) {
+			if(peeps.get(i) > heighest) {
+				heighest = peeps.get(i);
+			}
+		}
+		return heighest;
+	}
+	
+	
+	public static String findLongestWord(List<String> words) {
+		String longest = "a";
+		
+		for (int i = 0; i < words.size(); i++) {
+			if(words.get(i).length() > longest.length()) {
+				longest = words.get(i);
+			}
+		}
+		return longest;
+	}
+	
+	public static boolean containsSOS(List<String> messages) {
+		for (int i = 0; i < messages.size(); i++) {
+			if(messages.get(i).contains("... --- ...")) {
+				return true;
+			}
+		}
+		return false;
+		
+		
+	}
+	
+	public static double sortScores(List<Double> results) {
+		for (int i = 1; i < results.size(); i++) {
+		
+			for (int j = i; j > 0; j++) {
+	
+				if(results.get(j) < results.get(j - 1)) {
+					/*
+					int swap = results.get(j);
+					
+					results.get(j) = array[i];					
+					results.get(i) = swap;
+					*/
+				}
+			}
+				
+		}
+		return 0;
+	}
+	
+	
 }
--- src/_02_More_Algorithms/_00_MoreAndMoreTests.java
+++ src/_02_More_Algorithms/_00_MoreAndMoreTests.java
@@ -10,52 +10,90 @@ import org.junit.Test;
 
 public class _00_MoreAndMoreTests {
 
-//	@Test
-//	  public void MultTest() {
-//
-//	    assertEquals("10 x 0 = 0", multiply(10, 0));
-//	    assertEquals("10 x 10 = 100", multiply(10, 10));
-//	    assertEquals("8 x 11 = 88", multiply(8, 11));
-//	  }
-//
-//	@Test
-//	  public void PrimeTest() {
-//
-//	    assertTrue(isPrime(3));
-//	    assertTrue(isPrime(5));
-//	    assertTrue(isPrime(541));
-//	    assertFalse(isPrime(4));
-//	    assertFalse(isPrime(12));
-//	    assertFalse(isPrime(527));
-//
-//	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
+	@Test
+	  public void MultTest() {
+
+	    assertEquals("10 x 0 = 0", multiply(10, 0));
+	    assertEquals("10 x 10 = 100", multiply(10, 10));
+	    assertEquals("8 x 11 = 88", multiply(8, 11));
+	  }
+	
+	int multiply(int one, int two) {
+		return one * two;
+	}
+
+	@Test
+	  public void PrimeTest() {
+
+	    assertTrue(isPrime(3));
+	    assertTrue(isPrime(5));
+	    assertTrue(isPrime(541));
+	    assertFalse(isPrime(4));
+	    assertFalse(isPrime(12));
+	    assertFalse(isPrime(527));
+
+	  }
+	
+	boolean isPrime(int num) {
+		int remainder;
+      for(int i=2;i<=num/2;i++) {
+       
+    	           remainder=num%i;
+    	            
+    	     if(remainder == 0) {
+    	     
+    	        return false;
+    	     }
+    	     else {
+    	    	return true;
+    	  }
+
+    	    }
+    	  return false;
+	}
+
+	@Test
+	  public void SquareTest() {
+
+	    assertTrue(isSquare(4));
+	    assertTrue(isSquare(144));
+	    assertTrue(isSquare(64));
+	    assertTrue(isSquare(10201));
+	    assertTrue(isSquare(1));
+	    assertFalse(isSquare(3));
+	    assertFalse(isSquare(22));
+	    assertFalse(isSquare(143));
+
+	  }
+	
+	boolean isSquare(int num) {
+		int x = (int) Math.sqrt(num);
+		if(Math.pow(x,2) == num) {
+			return true;
+		}
+		return false;
+	}
+
+	@Test
+	  public void CubeTest() {
+
+	    assertTrue(isCube(27));
+	    assertTrue(isCube(216));
+	    assertTrue(isCube(729));
+	    assertTrue(isCube(1));
+	    assertFalse(isCube(3));
+	    assertFalse(isCube(22));
+	    assertFalse(isCube(143));
+
+	  }
+	
+	boolean isCube(int num) {
+		int x = (int) Math.sqrt(num);
+		if(Math.pow(x,3) == num) {
+			return true;
+		}
+		return false;
+	}
 
 
 
